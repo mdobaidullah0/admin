a397b2689452c4e8a28e8e3b958f25a71f5d82ce
mdobaidullah0 <mdobaidullah1222@gmail.com>
2026-03-23 16:14:23 +0600

    firebase_config.js

--- js/firebase_config.js
+++ js/firebase_config.js
@@ -2,15 +2,16 @@
 // IMPORTANT: Replace these placeholder values with your actual Firebase project credentials
 // Get your config from: Firebase Console > Project Settings > Your Apps > Web App
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBi795FLtS4Y3l3LUgzhNcH2ewIrMxGf3w",
-  authDomain: "red-rat-f758d.firebaseapp.com",
-  databaseURL: "https://red-rat-f758d-default-rtdb.firebaseio.com",
-  projectId: "red-rat-f758d",
-  storageBucket: "red-rat-f758d.firebasestorage.app",
-  messagingSenderId: "477883679691",
-  appId: "1:477883679691:web:a9455fca004cb52a29ec4e",
-  measurementId: "G-4793M8FM3D"
+  apiKey: "AIzaSyD0tQPx_arK42qNdmrQjxcrwMoraq0Y5m4",
+  authDomain: "rat-1806e.firebaseapp.com",
+  databaseURL: "https://rat-1806e-default-rtdb.firebaseio.com",
+  projectId: "rat-1806e",
+  storageBucket: "rat-1806e.firebasestorage.app",
+  messagingSenderId: "188519430980",
+  appId: "1:188519430980:web:46270e90aa86367758f511",
+  measurementId: "G-6PHD8SFP1P"
 };
 
 // Export for use in other files if using modules, or just global scope for simple script tags
